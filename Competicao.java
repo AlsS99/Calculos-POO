270b5b9a128430c3721d158d713b2914ecce8bc0
Larice <annelarice93@gmail.com>
2025-01-14 18:08:00 -0300

    Create Competicao.java

---
+++ Competicao.java
@@ -0,0 +1,19 @@
+package PescaEs;
+
+public class Competicao {
+    String nome;
+    String data;
+    Local local; //dependencia()
+
+    Competicao(String nome, String data, Local local) {
+         this.nome = nome;
+         this.data = data;
+         this.local = local;
+
+}
+
+public void cadastrarCompeticao(){
+
+}
+
+}
